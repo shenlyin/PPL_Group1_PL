e857c07047ae469c70f1375a8496e6ce2a7d5057
ConnorSp0 <122158578+ConnorSp0@users.noreply.github.com>
2023-01-07 21:20:38 +0800

    Naghaha

--- Lexer_Klak.py
+++ Lexer_Klak.py
@@ -1,5 +1,5 @@
 DIGITS = "0123456789"      #Character Sets Comparisons
-LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzHAHA"
 OSYMBOLS = "~`!@#$&_\\:"
 IDLIST = DIGITS + LETTERS + "_"
 
